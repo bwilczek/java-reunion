1bce838088a02690cb275bc7c5ac23de380e67b8
Bartek Wilczek <bwilczek@gmail.com>
2017-08-06 19:26:12 +0200

    Introduced testng

--- src/main/java/org/vouk/reunion/ConsoleDemo.java
+++ src/main/java/org/vouk/reunion/ConsoleDemo.java
@@ -2,8 +2,9 @@ package org.vouk.reunion;
 
 public class ConsoleDemo {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello World 1");
+    System.out.println("Hello World 2");
+  }
 
 }
---
+++ src/test/java/org/vouk/reunion/test/ConsoleDemoTest.java
@@ -0,0 +1,12 @@
+package org.vouk.reunion.test;
+
+import org.testng.annotations.*;
+
+public class ConsoleDemoTest {
+
+  @Test
+  public void testMethod1() {
+    assert true;
+  }
+
+}
